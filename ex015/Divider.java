628a03ce6cc7187955162e090e2752a15d0a9340
Kyle8584 <kylez10@educbe.ca>
2026-03-30 13:13:09 -0600

    new

---
+++ ex015/Divider.java
@@ -0,0 +1,8 @@
+public class Divider {
+    public static double divide(int numerator, int denominator){
+        if (denominator == 0){
+            throw new ArithmeticException("division by zero");
+        }
+        return (double) numerator / denominator;
+    }
+}
