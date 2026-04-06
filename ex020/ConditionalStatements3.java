f47e81a414afb420a70b47a938403fe2c86b0de1
Kyle8584 <kylez10@educbe.ca>
2026-04-06 13:26:32 -0600

    dog

--- ex020/ConditionalStatements3.java
+++ ex020/ConditionalStatements3.java
@@ -1,3 +1,11 @@
 public class ConditionalStatements3 {
-    
+    public static String getAnimal(String breed){
+        if (breed.equals("husky") || breed.equals("shepherd") || breed.equals("beagle")){
+            return "dog";
+        } else if (breed.equals("siamese") || breed.equals("persian") || breed.equals("sphynx")){
+            return "cat";
+        } else {
+            return "other";
+        }
+    }
 }
